30457542311e21af9101751fbe51a00fb59391e4
putriaulianti <42660888+putriaulianti@users.noreply.github.com>
2018-08-24 10:42:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-//tuliskan loopingnya di sini
+	for($b=1;$b<=5;$b++) {
+		for($a=$b;$a<=5;$a++) {
+			if ($b<=5){
+				echo "*";
+			}
+			else {
+				echo " ";
+			}
+		}
+		echo "<br>";
+	}
 ?>
